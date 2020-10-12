8759e64cea20e90bbd26673ad4580f290b2e392d
Dyllon Gunawardhana <dyllon.g@outlook.com>
2020-10-12 15:54:08 +0800

    init commit

---
+++ Lab 1/src/printgrades.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void printgrades()
+{
+    int studentNumber = 0, mark;
+
+    printf("Enter Student ID: \n");
+    scanf("%d", &studentNumber);
+
+    while (studentNumber != -1)
+    {
+        printf("Enter Mark: ");
+        scanf("%d", &mark);
+
+        switch (mark)
+        {
+
+        case 75 ... 100:
+            printf("Grade = A\n");
+            break;
+        case 65 ... 74:
+            printf("Grade = B\n");
+            break;
+        case 55 ... 64:
+            printf("Grade = C\n");
+            break;
+        case 45 ... 54:
+            printf("Grade = D\n");
+            break;
+        default:
+            printf("Grade = F\n");
+            break;
+        }
+
+        printf("Enter Student ID: ");
+        scanf("%d", &studentNumber);
+    }
+    // return 0;
+}
