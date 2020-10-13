e796c762259099b21cf3d2b30d2d52213605a52d
Dyllon Gunawardhana <contact@dyllon.dev>
2020-10-13 12:02:31 +0800

    add broken part2/lab1 code

---
+++ Part 2 - C/Lab 1/printaverage.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int printaverage() {
+	int num_lines, num_input, num_count = 0, num_total = 0;
+	int num_average = 0;
+
+	printf("Enter the number of lines:\n");
+	scanf("%d", &num_lines);
+
+  int i;
+	for (i = 0; i < num_lines; ++i) 
+	{
+	  printf("[line%d] Enter the numbers:\n", i+1);
+	scanf("%d", &num_input);
+
+	while (num_input != -1) {
+		num_total += num_input;
+		num_count++;
+		// printf("input: %d, total: %d, count: %d\n", num_input, num_total,
+		// num_count);
+
+		scanf("%d", &num_input);
+	}
+
+	};
+
+		
+	num_average = num_total / num_count;
+	printf("average is: %d", num_average);
+
+	return 0;
+}
